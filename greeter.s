e5a0fa839ebaa2ffc579e139ef6fc58c631d6de6
Colin Ashburn <cashburn@purdue.edu>
2015-10-18 17:56:41 -0400

    2015-10-18

--- greeter.s
+++ greeter.s
@@ -2,15 +2,16 @@
 .section .data
 		arg1: .asciz "%s"
 		arg2: .space 100
-		string: .asciz "Hello, World!"
+		string: .asciz "Hello, World!\n"
 .section .text
 .global	main
 main:
 	push {ip, lr}
 	ldr r0, =string
 	bl printf
-
 	pop {ip, pc}
-mov r7, $1
-svc $0
+
+	mov     %r0, $0     /* status -> 0 */
+	mov r7, $1
+	svc $0
 .end
