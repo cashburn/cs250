187de1523a62bdcb8e220a8457c0cbe1826dfc07
Colin Ashburn <cashburn@purdue.edu>
2015-10-18 18:25:56 -0400

    2015-10-18

--- greeter.s
+++ greeter.s
@@ -1,16 +1,27 @@
 @greeter.s
 .section .data
-		arg1: .asciz "%s"
-		arg2: .space 100
-		string: .asciz "Hello, World!\n"
+		inputFormat: .asciz "%s"
+		input: .space 100
+		progName .asciz "** Greeter **\n"
+		prompt .asciz "Please enter your name: "
+		greeting .asciz "Hello %s"
 .section .text
 .global	main
 main:
 	push {ip, lr}
-	ldr r0, =string
+	ldr r0, =progName
 	bl printf
+	ldr r0, =prompt
+	bl printf
+	ldr r0, =inputFormat
+	ldr r1, =input
+	bl scanf
+	ldr r0, =greeting
+	ldr r1, =input
+	bl printf
+
 	pop {ip, pc}
 
 	mov r0, $0
 	mov r7, $1
-	swi 0x11
+	swi $0
