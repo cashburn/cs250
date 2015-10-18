38fe8b57b3f44f3a98761050c1c258e1616f0254
Colin Ashburn <cashburn@purdue.edu>
2015-10-18 16:58:21 -0400

    2015-10-18

--- greeter.s
+++ greeter.s
@@ -1,14 +1,9 @@
 @greeter.s
 @demo program
 .section .data
-		arg1: .asciz "%s"
-		arg2: .space 100
-		string: .asciz "Hello World!"
 .section .text
 .globl	_start
 _start:
-ldr r0, =string
-bl printf
 
 mov r7, $1
 svc $0
