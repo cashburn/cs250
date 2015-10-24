6329165c7759be185853e53553d3d18bce6802ee
Colin Ashburn <cashburn@purdue.edu>
2015-10-24 06:14:17 +0000

    2015-10-24

--- pwm.c
+++ pwm.c
@@ -1,3 +1,4 @@
+ ln -s "$HOME/.dot/background.png" "$HOME/Pictures/background.png"   #Just a nice photo I took that I like to use for backgrounds.  TODO: Comment out and delete the picture in .dot if you want.
 #include <stdio.h>
 #include <wiringPi.h>
 #include <softPwm.h>
@@ -9,25 +10,49 @@
 #define BUTTONG 24
 #define BUTTONB 23
 int main(void) {
-	if(wiringPiSetup() == -1)
+	if(wiringPiSetup() == -1)       //begin setup()
         return 1;
-    pinMode(LEDR, OUTPUT);
+    pinMode(LEDR, OUTPUT);          //set I/O pins
     pinMode(LEDG, OUTPUT);
     pinMode(LEDB, OUTPUT);
     pinMode(BUTTONR, INPUT);
     pinMode(BUTTONG, INPUT);
     pinMode(BUTTONB, INPUT);
-    int value[3];
-    int red = 0;
-    int green = 0;
-    int blue = 0;
-    while(1) {
-        red = digitalRead(BUTTONR);
-        green = digitalRead(BUTTONG);
-        blue = digitalRead(BUTTONB);
-        digitalWrite(LEDR, red);
-        digitalWrite(LEDG, green);
-        digitalWrite(LEDB, blue);
+    pinMode(PLUS, INPUT);
+    pinMode(MINUS, INPUT);
+
+    softPwmCreate(LEDR, 0, 100);    //start PWM from 0 to 100
+    softPwmCreate(LEDG, 0, 100);
+    softPwmCreate(LEDB, 0, 100);
+    
+    int selector = 0;               //color selector
+    int value[3];                   //color values: R=0,G=1,B=2
+    int changed;                    //set when the plus5 or minus5 buttons are used
+    for (int 0; i < 2; i++) {
+        value[i] = 0;
+    }
+
+    while(1) {                      //begin loop()
+        changed = 0;
+        if (digitalRead(BUTTONR))   //select color
+            selector = 0;
+        if (digitalRead(BUTTONG))
+            selector = 1;
+        if (digitalRead(BUTTONB))
+            selector = 2;
+ 
+        if (digitalread(PLUS)) {    //read plus5/minus5
+            value[selector] += 5;
+            changed = 1;
+        }
+        if (digitalread(MINUS)) {
+            value[selector] -= 5;
+            changed = 1;
+        }
+        if (changed) {              //write values if changed
+            softPwmWriteWrite(LEDR, value[0]);
+            softPwmWriteWrite(LEDG, value[1]);
+            softPwmWriteWrite(LEDB, value[2]);
         
         delay(100);
     }
