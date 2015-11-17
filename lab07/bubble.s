83ad4924ab20fafe335d553363a5ba3312097cfc
Colin Ashburn <cashburn@purdue.edu>
2015-11-17 06:20:41 +0000

    2015-11-17

--- lab07/bubble.s
+++ lab07/bubble.s
@@ -1,32 +1,30 @@
 @bubble.s
 .equ dataSize, 4
 .global bubble
-@modify here
 bubble:
     push {ip, lr}
 	
-    mov r4, $0
-    sub r2, r1, $1
+    mov r4, $0              //i = 0 (uses ++i later)
+    sub r2, r1, $1          //r2 = n - 1
 
 loopi:
-    add r4, r4, $1
-    sub r6, r0, $dataSize
-    mov r5, $-1
-    cmp r4, r1
-    bge end
-loopj:
-    add r5, r5, $1
-    cmp r5, r2
-    bge loopi
+    add r4, r4, $1          //++i
+    sub r6, r0, $dataSize   //aPoint = a - 4 (uses ++r6 later)
+    mov r5, $-1             //j = -1 (uses ++j later)
+    cmp r4, r1              
+    bge end                 
+loopj:                      //if i < n
+    add r5, r5, $1          //++j
+    cmp r5, r2              
+    bge loopi               // if !(j < n-1) cont for loop 1
     
-    add r6, r6, $dataSize
-    //add r7, r6, $dataSize
-    ldr r8, [r6]
-    ldr r9, [r6, $dataSize]
-    cmp r9, r8
-    bge loopj
-    swp r9, r9, [r6]
-    str r9, [r6, $dataSize]
+    add r6, r6, $dataSize   //++aPoint
+    ldr r8, [r6]            //a[j]
+    ldr r9, [r6, $dataSize] //a[j+1}
+    cmp r9, r8              
+    bge loopj               //if !(a[j+1] < a[j]) cont for loop 2
+    swp r9, r9, [r6]        //swap a[j] and a[j+1]
+    str r9, [r6, $dataSize] //finish swap
     b loopj
     
 
